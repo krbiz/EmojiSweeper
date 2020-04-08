574d136e63850a336d5ff5c32dc7071ba3cb09cb
Dima Krupskyi <krupskydima@gmail.com>
2020-04-08 20:37:26 +0300

    Add RestultCell

---
+++ EmojiSweeper/Views/ResultCell.swift
@@ -0,0 +1,24 @@
+//
+//  ResultCell.swift
+//  EmojiSweeper
+//
+//  Created by Dima Krupskyi on 08.04.2020.
+//  Copyright © 2020 Dima Krupskyi. All rights reserved.
+//
+
+import UIKit
+
+class ResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
